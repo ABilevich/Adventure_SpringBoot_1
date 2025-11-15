fdfc7fcf7a6bbe2f6841f1d6cb5f6b531f77a731
Andres Bilevich <andresbilevich@gmail.com>
2025-11-15 13:13:19 +0100

    chore: updated project structure

---
+++ 02_SpringBootSpringCore/src/main/java/com/andy/sprigcoredemo/SprigcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.andy.sprigcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigcoredemoApplication.class, args);
+	}
+
+}
