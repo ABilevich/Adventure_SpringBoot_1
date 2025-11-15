ba717adeeeded9be2e8536f793678b1ca2100bac
Andres Bilevich <andresbilevich@gmail.com>
2025-11-15 16:04:22 +0100

    testing prototype scope

--- 02_SpringBootSpringCore/src/main/java/com/andy/sprigcoredemo/rest/DemoController.java
+++ 02_SpringBootSpringCore/src/main/java/com/andy/sprigcoredemo/rest/DemoController.java
@@ -11,16 +11,26 @@ import com.andy.sprigcoredemo.common.Coach;
 public class DemoController {
     // Define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // Define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController(
+        @Qualifier("cricketCoach") Coach theCoach,
+        @Qualifier("cricketCoach") Coach theAnotherCoach
+    ){
         System.out.println("In Constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
     
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach==anotherCoach: " + (myCoach == anotherCoach);
     }
 }
